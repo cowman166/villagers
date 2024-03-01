4b1ba6dea832d79d90d30635dc7de5263a6f12a5
Andre Montgomery <andremontgomery166@gmail.com>
2024-02-29 17:44:44 -0700

    doneee

--- villager_data.py
+++ villager_data.py
@@ -23,7 +23,7 @@ def all_species(filename):
     file.close()
     return species
 
-print(all_species("villagers.csv"))
+
 
 def get_villagers_by_species(filename, search_string="All"):
     """Return a list of villagers' names by species.
@@ -35,12 +35,22 @@ def get_villagers_by_species(filename, search_string="All"):
     Return:
         - list[str]: a list of names
     """
-
+    file = open(filename)
     villagers = []
-
+    for line in file:
+        if search_string == "All":
+            name_temp, species, personality, hobby, motto = line.split('|')
+            villagers.append(name_temp)
+        else:
+            name_temp, species, personality, hobby, motto = line.split('|')
+            if search_string == species:
+                villagers.append(name_temp)
+    
+    file.close()
     # TODO: replace this with your code
 
     return sorted(villagers)
+
 
 
 def all_names_by_hobby(filename):
@@ -53,10 +63,31 @@ def all_names_by_hobby(filename):
         - list[list[str]]: a list of lists containing names
     """
 
-    # TODO: replace this with your code
+    fitness = []
+    nature = []
+    education = []
+    music = []
+    fashion = []
+    play = []
 
-    return []
+    file = open(filename)
 
+    
+    villagers = [[], [], [], [], [], []]
+    hobbies = ["Fitness", "Nature", "Education", "Music", "Fashion", "Play"]
+    for line in file:
+        name_temp, species, personality, hobby_temp, motto = line.split('|')
+        for i, hobby in enumerate(hobbies):
+            if hobby == hobby_temp:
+                villagers[i].append(name_temp)    
+    file.close()
+
+    return [
+        sorted(villagers[0]), sorted(villagers[1]), 
+        sorted(villagers[2]), sorted(villagers[3]), 
+        sorted(villagers[4]), sorted(villagers[5])]
+
+#print(all_names_by_hobby("villagers.csv"))
 
 def all_data(filename):
     """Return all the data in a file.
@@ -70,13 +101,19 @@ def all_data(filename):
     Return:
         - list[tuple[str]]: a list of tuples containing strings
     """
-
+    file = open(filename)
     all_data = []
+
+    for line in file:
+        name, species, personality, hobby, motto = line.split('|')
+        all_data.append((name, species, personality, hobby, motto))
+    file.close()
 
     # TODO: replace this with your code
 
     return all_data
 
+#print(all_data("villagers.csv"))
 
 def find_motto(filename, villager_name):
     """Return the villager's motto.
@@ -93,7 +130,18 @@ def find_motto(filename, villager_name):
     """
 
     # TODO: replace this with your code
+    file = open(filename)
 
+    for line in file:
+        name, species, personality, hobby, motto = line.split('|')
+        if name == villager_name:
+            file.close()
+            return motto
+        
+    file.close()
+    return None
+
+#print(find_motto("villagers.csv", "haoidjfio") )
 
 def find_likeminded_villagers(filename, villager_name):
     """Return a set of villagers with the same personality as the given villager.
@@ -109,5 +157,29 @@ def find_likeminded_villagers(filename, villager_name):
         >>> find_likeminded_villagers('villagers.csv', 'Wendy')
         {'Bella', ..., 'Carmen'}
     """
+    personality = ""
+    file = open(filename)
+
+    for line in file:
+        name, species, personality_temp, hobby, motto = line.split('|')
+        if name == villager_name:
+            personality = personality_temp
+    file.close()
+
+    file = open(filename)
+    villagers = []
+
+    for line in file:
+        name, species, personality_temp, hobby, motto = line.split('|')
+        if personality == personality_temp:
+            villagers.append(name)
+    file.close()
+
+    input_name = {villager_name}
+    villagers = set(villagers)
+
+    return villagers ^ input_name
+
+print(find_likeminded_villagers("villagers.csv", "Skye"))
 
     # TODO: replace this with your code
